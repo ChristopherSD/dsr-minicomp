450c948ff84813712381daca7299c494cd538995
michi-d <drews@neuro.mpg.de>
2020-07-20 11:29:12 +0200

    add data_helpers

---
+++ data_helpers.py
@@ -0,0 +1,13 @@
+'''Data helper functions.
+'''
+
+import pandas as pd
+
+def get_all_train_data():
+    '''Get all data an merge into one big dataframe
+    '''
+    train_data = pd.read_csv("data/train.csv")
+    store_data = pd.read_csv("data/store.csv")
+
+    all_data = train_data.merge(store_data, how = 'outer', left_on='Store', right_on='Store')
+    return all_data
