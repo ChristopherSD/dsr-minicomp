542180d5582d68d42e1fdb18f5045ec884ec63fe
Chris <e0946375@student.tuwien.ac.at>
2020-07-20 16:29:29 +0200

    Add function to impute missing DayOfWeek from Date

--- data_transformation.py
+++ data_transformation.py
@@ -1,5 +1,5 @@
-'''Function to impute and transform data.
-'''
+"""Functions to impute and transform data.
+"""
 from typing import List
 
 import pandas as pd
@@ -7,8 +7,8 @@ import pandas as pd
 _sales_col = 'Sales'
 
 def get_all_train_data():
-    '''    Get all data an merge into one big dataframe
-    '''
+    """Get all data an merge into one big dataframe and convert Date to datetime
+    """
     train_data = pd.read_csv("data/train.csv")
     store_data = pd.read_csv("data/store.csv")
 
@@ -19,7 +19,7 @@ def get_all_train_data():
 
 
 def fillna_StoreType_and_factorize(all_data):
-    '''Fill nan value in StoreType with string 'unknown' and label encode the values
+    """Fill nan value in StoreType with string 'unknown' and label encode the values
 
     Args:
         all_data: Raw data
@@ -27,7 +27,7 @@ def fillna_StoreType_and_factorize(all_data):
         output: New column
         int_to_storetype: Decoding dictionary
         storetype_to_int: Encoding dictionary
-    '''
+    """
 
     output = all_data.StoreType.fillna('unknown')
     storetype_to_int = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'unknown': 0}
@@ -39,6 +39,30 @@ def fillna_StoreType_and_factorize(all_data):
 
 
 def drop_empty_sales(df: pd.DataFrame) -> pd.DataFrame:
-    '''Drop all rows where the Sales column is Na.
-    '''
+    """Drop all rows where the Sales column is Na.
+    """
     return df[df[_sales_col].notna()]
+
+
+def impute_dayofweek_from_date(df: pd.DataFrame, date_col='Date', dow_col='DayOfWeek') -> pd.Series:
+    """ Impute the day of the week from the corresponding date, if present.
+    If the date column is not of type DateTime, it will be converted locally (the original data frame is not changed).
+    If the corresponding date entry is not available, day of week will remain Na.
+
+    Args:
+        df: original raw data
+        date_col: The name of the column containing the date (as String or DateTime)
+        dow_col: The name of the column containing the day of week (as float)
+    Returns:
+        dow_imputed: A pd.Series representing the day of week with imputed values.
+    """
+    missing_dow = df.loc[df[dow_col].isna()]
+    if not pd.api.types.is_datetime64_any_dtype(missing_dow[date_col]):
+        missing_dow[date_col] = pd.to_datetime(missing_dow[date_col])
+
+    missing_dow[dow_col] = missing_dow[date_col].dt.dayofweek
+    # add one to dt.dayofweek since the original original feature values start with Monday == 1,
+    # whereas DateTime starts with Monday == 0
+    dow_imputed = df[dow_col].fillna(missing_dow[dow_col]) + 1.0
+
+    return dow_imputed
