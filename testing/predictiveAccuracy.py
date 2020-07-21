f8c2f37b7ced9bdeeedacbffd3db514b4a2402e5
Chris <e0946375@student.tuwien.ac.at>
2020-07-21 20:05:15 +0200

    Add Chris' personal stuff on personal branch

--- testing/predictiveAccuracy.py
+++ testing/predictiveAccuracy.py
@@ -6,3 +6,9 @@ def metric(preds, actuals):
     actuals = actuals.reshape(-1)
     assert preds.shape == actuals.shape
     return 100 * np.linalg.norm((actuals - preds) / actuals) / np.sqrt(preds.shape[0])
+
+def custom_scorer_metric(actuals, preds):
+    preds = preds.reshape(-1)
+    actuals = actuals.reshape(-1)
+    assert preds.shape == actuals.shape
+    return 100 * np.linalg.norm((actuals - preds) / actuals) / np.sqrt(preds.shape[0])
