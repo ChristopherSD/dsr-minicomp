a11853f42bca7bb8e3e3627ce9cdd95836efc32a
Chris <e0946375@student.tuwien.ac.at>
2020-07-21 11:58:52 +0200

    Add function to generate PromoStarted feature from PromoInterval
    Works inplace and drops PromoInterval.

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import datetime as datetime
 import numpy as np
+from dateutil.parser import parse
+
 
 def generate_CompetitionSince(all_data: pd.DataFrame):
     """Generate (inplace) a feature 'CompetitionSince' which counts the months (in integer) since
@@ -17,6 +19,29 @@ def generate_CompetitionSince(all_data: pd.DataFrame):
 
     all_data.loc[mask, 'CompetitionSince'] = CompetitionSince
 
+
 def execute_transformations(df: pd.DataFrame) -> pd.DataFrame:
 
     return df.dropna(axis=1)
+
+
+def is_in_promo_month(row, itvl_col='PromoInterval'):
+    if (itvl_col in row) and isinstance(row[itvl_col], str):
+        intervals = row[itvl_col].split(',')
+        itvl_dates = list(map(parse, intervals))
+        for date in itvl_dates:
+            if row['Date'].month == date.month:
+                return 1.0
+
+    return 0.0
+
+
+def generate_PromoStarted(all_data: pd.DataFrame, itvl_col='PromoInterval'):
+    """Generate (inplace) a feature 'CompetitionSince' which counts the months (in integer) since
+    when the competition started.
+    """
+    new_col_name = 'PromoStarted'
+    promo_started = all_data.apply(is_in_promo_month, axis=1)
+    all_data[new_col_name] = promo_started
+
+    all_data.drop(labels=[itvl_col], axis=1, inplace=True)
